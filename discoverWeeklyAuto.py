19254c9d97dee018d6461412af1fda4d5086367e
Parth Bhavsar <parth@Parths-MacBook-Pro.local>
2022-06-18 21:43:09 -0400

    Added Archiving

--- discoverWeeklyAuto.py
+++ discoverWeeklyAuto.py
@@ -3,30 +3,49 @@ import base64
 from pprint import pprint
 import json
 
-clientID = "21e7bf05b9e34b8b9193cd132e238353"
-clientSecret = "59cbbf7ccef840b38b3b77411049d04a"
+#clientID = "21e7bf05b9e34b8b9193cd132e238353"
+#clientSecret = "59cbbf7ccef840b38b3b77411049d04a"
 
 base_uri = "https://api.spotify.com/v1"
+token = "Bearer BQDQkzd1vunbE-kMFGT0gRkWGcrPTK1X42N6oCMe71Z0ZmjKAU4vAq1_6b2PRVVEe3yCsxTWdNRjH8z_F1j4V4JzaIadHgV1wxeOx8taSR8wIEQAsKWqk0S3W9mZsjPzU_3eQixCc3QTZfYMzR1NKyywaMwPX7gB5uJnJqzyjaO8SWgu2-G0QDNGF83K-ceUAVvbGuO1ZA3FZFbapMSAzzRYPUyb1JxR0WoD9BVUGxYJVo7fahYz663Psvs5mbBYfIvNXAg"
+
+def authorize():
+    auth_header = {'Authorization' : "Basic MjFlN2JmMDViOWUzNGI4YjkxOTNjZDEzMmUyMzgzNTM6NTljYmJmN2NjZWY4NDBiMzhiM2I3NzQxMTA0OWQwNGE=",
+                    'Content-Type':'application/x-www-form-urlencoded'
+                    }
+    params = {'grant_type':'client_credentials'}
+    url = base_uri + "/api/token"
+    response = requests.post(url,headers=auth_header,params=params)
+    print(response.text)
 
 def discover_weekly():
 
     track_list = []
-    #auth_params = {"grant_type" : "client_credentials"}
-    auth_header = {'Authorization' : "Bearer BQD5G30pAIw1qCEV7hDCTJRbp9p9xV5MAr6puobwGUgixZhytLZbGuiS0vKiyZf8Geez2l28QQRufQTozvQBYmgpIw_aOvLP0726hiVus0dCiAC8pG4lEf5DndFy41hbCqw_iLtMiG98J1ieRJEVEROdGhQ6E-mqhaO8TzrB-RIcrYq7Ug4KZGRwxTL-T1DXDWuOKH_lsXHI",
+
+    auth_header = {'Authorization' : token,
                     'Content-Type':'application/json',
                     'Accept': 'application/json'}
     url = base_uri + "/playlists/37i9dQZEVXcGC5dYZXQeb1/tracks"
     response = requests.get(url,headers=auth_header)
-    #print(type(response.text))
     resp = json.loads(response.text)
-    #print(type(resp["items"]))
-    #print(resp["items"][0]["track"]["uri"])
-
+    #print(resp)
+    
     for i in resp["items"]:
-        track_list.append(((i["track"]["uri"]).split(":"))[2])
-    print(len(track_list))
+        track_list.append((i["track"]["uri"]))#.split(":"))[2])
+    return(track_list)
 
+def addToArchive(track_list):
+    
+    tracks = ",".join(track_list)
 
+    auth_header = {'Authorization' : token,
+                    'Content-Type':'application/json',
+                    'Accept': 'application/json'}
+    url = base_uri + "/playlists/2QhTgrWeLP4HMU1Ovq8QGa/tracks"
+    params = {'uris':tracks}
+    response = requests.post(url,headers=auth_header,params=params)
+    resp = json.loads(response.text)
+    print(resp)
 
 def test():
 
@@ -37,9 +56,10 @@ def test():
     print(sec)
 
 def main():
-    authorize()
+    x = discover_weekly()
     #test()
-    
+    #authorize()
+    addToArchive(x)
 
 if __name__ == "__main__":
     main()
